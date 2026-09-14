beb6eec2ae5585edab86bb5e2b015564072f635c
MohsanDevelpor786 <muhammadmohsanm366@gmail.com>
2026-09-14 11:59:00 +0500

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,132 @@
+<!-- Sidebar Start -->
+
+<div class="sidebar pe-4 pb-3">
+
+    <nav class="navbar bg-light navbar-light">
+
+        <!-- Logo -->
+        <div class="d-flex align-items-center">
+
+            <a href="index.php"
+                class="mt-2 d-flex align-items-center text-decoration-none">
+
+                <span
+                    class="ms-4 bg-primary text-white rounded-circle d-inline-flex align-items-center justify-content-center me-2"
+                    style="width:30px; height:30px;">
+
+                    <i class="bi bi-speedometer2"></i>
+
+                </span>
+
+                <h4 class="mb-0 text-primary fw-bold">
+                    Admin Side
+                </h4>
+
+            </a>
+
+        </div>
+
+
+        <!-- Admin Profile -->
+        <div class="d-flex align-items-center ms-4 mb-4 mt-4">
+
+            <div class="position-relative">
+
+                <img class="rounded-circle"
+                    src="img/Mohsan.jpg"
+                    alt=""
+                    style="width: 40px; height: 40px;">
+
+                <div
+                    class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1">
+                </div>
+
+            </div>
+
+            <div class="ms-3">
+
+                <h6 class="mb-0">
+                    Mohsan Shakir
+                </h6>
+
+                <span>Admin</span>
+
+            </div>
+
+        </div>
+
+
+        <!-- Sidebar Menu -->
+
+        <div class="navbar-nav w-100">
+
+            <a href="index.php"
+                class="nav-item nav-link active">
+
+                <i class="fa fa-tachometer-alt me-2"></i>
+                Dashboard
+
+            </a>
+
+
+            <!-- Categories -->
+
+            <a href="categories.php"
+                class="nav-item nav-link">
+
+                <i class="fa fa-list me-2"></i>
+                Categories
+
+            </a>
+
+
+            <!-- Foods -->
+
+            <a href="foods.php"
+                class="nav-item nav-link">
+
+                <i class="fa fa-utensils me-2"></i>
+                Foods
+
+            </a>
+
+
+            <!-- Orders -->
+
+            <a href="orders.php"
+                class="nav-item nav-link">
+
+                <i class="fa fa-shopping-cart me-2"></i>
+                Orders
+
+            </a>
+
+
+            <!-- Users -->
+
+            <a href="users.php"
+                class="nav-item nav-link">
+
+                <i class="fa fa-users me-2"></i>
+                Users
+
+            </a>
+
+
+            <!-- Dashboard -->
+
+            <a href="dashboard.php"
+                class="nav-item nav-link">
+
+                <i class="fa fa-chart-bar me-2"></i>
+                Statistics
+
+            </a>
+
+        </div>
+
+    </nav>
+
+</div>
+
+<!-- Sidebar End -->
